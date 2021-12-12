f596da5c70555c85b2f9c365660173f917228a18
tamaskis <tamas.a.kis@outlook.com>
2021-12-12 13:23:38 -0800

    backup

--- res/Numerical_Differentiation_Toolbox/src/complexified/iatan2d.m
+++
@@ -1,8 +0,0 @@
-% https://mdolab.engin.umich.edu/misc/files/complexify.f90
-function result = iatan2d(y,x)
-    a = real(y);
-    b = imag(y);
-    c = real(x);
-    d = imag(x);
-    result = (180/pi)*atan2(a,c)+1i*((c*b-a*d)/(a^2+c^2));
-end
